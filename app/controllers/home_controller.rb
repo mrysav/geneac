81d731e1b4b059d3cec8b83f8cf52558aa41d892
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2018-09-08 15:00:33 -0400

    Add homepage recent photos carousel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@
 
 # Controller for any 'home' actions
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @recent_photos = Photo.order('updated_at desc').limit(3)
+  end
 end
--- app/dashboards/photo_dashboard.rb
+++ app/dashboards/photo_dashboard.rb
@@ -23,7 +23,6 @@ class PhotoDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
     :image,
     :title,
     :description,
