279fd2c18de75b425e522f35e653a0bd6b3fd6c4
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-03 23:03:13 -0500

    add bio to person in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_145605) do
+ActiveRecord::Schema.define(version: 2018_10_10_004647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2018_07_21_145605) do
     t.integer "current_spouse_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
   end
 
   create_table "photos", force: :cascade do |t|
