03cf99ae4bc353f6389d3bbb4175b1ee1c1c1eef
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-03 22:34:11 -0500

    Remove id from note dashboard

--- app/dashboards/note_dashboard.rb
+++ app/dashboards/note_dashboard.rb
@@ -23,7 +23,6 @@ class NoteDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
     :title,
     :date,
     :content,
