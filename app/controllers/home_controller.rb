a0e5253e2935aab7f0818391dc8dedce14551d94
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-19 00:25:28 -0500

    Add /tagged/ route and share search partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,29 +3,14 @@
 # Controller for any 'home' actions
 class HomeController < ApplicationController
   def index
-    # @recent_photos = Photo.order('updated_at desc').limit(3)
     @recent_updates = []
-    Person.order('updated_at desc').limit(5).each do |p|
-      @recent_updates.push(
-        title: p.full_name,
-        updated_at: p.updated_at,
-        model: p
-      )
-    end
-    Note.order('updated_at desc').limit(5).each do |n|
-      @recent_updates.push(
-        title: n.title,
-        updated_at: n.updated_at,
-        model: n
-      )
-    end
-    Photo.order('updated_at desc').limit(5).each do |p|
-      @recent_updates.push(
-        title: p.title,
-        updated_at: p.updated_at,
-        model: p
-      )
-    end
-    @recent_updates.sort_by! { |r| r[:updated_at] }.reverse!
+    Person.order('updated_at desc').limit(5)
+          .each { |p| @recent_updates.push(p) }
+    Note.order('updated_at desc').limit(5)
+        .each { |n| @recent_updates.push(n) }
+    Photo.order('updated_at desc').limit(5)
+         .each { |p| @recent_updates.push(p) }
+
+    @recent_updates.sort_by!(&:updated_at).reverse!
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,14 @@ class SearchController < ApplicationController
   include PgSearch
 
   def search
-    @results = PgSearch.multisearch(params[:s])
+    @results = PgSearch.multisearch(params[:s]).map(&:searchable)
+  end
+
+  def tagged
+    tag = params[:tag]
+    @results = []
+    Note.tagged_with(tag).each { |n| @results.push(n) }
+    Photo.tagged_with(tag).each { |p| @results.push(p) }
+    render 'search'
   end
 end
