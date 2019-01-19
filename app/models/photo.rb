605108b2e0bb5894bae2c06772dd5b1936a687e0
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-19 01:37:31 -0500

    Add tags to photos and separate tag list into its own partial

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,4 +10,17 @@ class Photo < ApplicationRecord
   acts_as_taggable
 
   has_one_attached :image
+
+  def date
+    Date.edtf(super)
+  end
+
+  def date=(value)
+    parsed_date = Date.edtf(value)&.edtf
+    super(parsed_date)
+  end
+
+  def render_html
+    CommonMarker.render_html(description)
+  end
 end
