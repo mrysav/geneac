03e5a446f39785359220abda6552ccff212f925d
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-19 00:49:16 -0500

    add note model tests

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,6 +9,15 @@ class Note < ApplicationRecord
 
   acts_as_taggable
 
+  def date
+    Date.edtf(super)
+  end
+
+  def date=(value)
+    parsed_date = Date.edtf(value)&.edtf
+    super(parsed_date)
+  end
+
   def render_html
     CommonMarker.render_html(content)
   end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'note renders html from markdown' do
+    assert_equal "<p><em>Lorem Ipsum</em></p>\n", notes(:markdown).render_html
+  end
+
+  test 'note handles edtf dates correctly' do
+    note = notes(:no_date)
+    assert_nil note.date
+    note.date = '2010-10-31'
+    assert_equal 2010, note.date.year
+    note.date = '10/31/2010'
+    assert_nil note.date
+  end
 end
