ab2ab24c312402475b1033535e046406e90e16b5
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-26 02:19:49 -0500

    updates to notes and photo views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,5 +4,11 @@
 class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
+    authorize @photo
+
+    @tagged_people = @photo.tagged_people.map do |p|
+      id = p.name.to_i
+      Person.find(id) if Person.exists?(id)
+    end
   end
 end
