2d52c70767db2f818ffff9322eeb513229468846
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-01-26 20:03:26 -0500

    added photo model tests

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'photo handles edtf dates correctly' do
+    photo = photos(:no_image)
+    assert_nil photo.date
+    photo.date = '2010-10-31'
+    assert_equal 2010, photo.date.year
+    photo.date = '10/31/2010'
+    assert_nil photo.date
+  end
 end
