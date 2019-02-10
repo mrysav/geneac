97b4a7b66f674def1cac4995ae70f81b96869cd8
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-02-10 01:01:01 -0500

    hide recent updates based on settings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,28 @@
 # Controller for any 'home' actions
 class HomeController < ApplicationController
   def index
-    @recent_updates = []
-    Person.order('updated_at desc').limit(5)
-          .each { |p| @recent_updates.push(p) }
-    Note.order('updated_at desc').limit(5)
-        .each { |n| @recent_updates.push(n) }
-    Photo.order('updated_at desc').limit(5)
-         .each { |p| @recent_updates.push(p) }
-
-    @recent_updates.sort_by!(&:updated_at).reverse!
-    authorize_list @recent_updates
-    @recent_updates = @recent_updates[0..4]
+    if Setting.show_recent_updates
+      @recent_updates = recent_updates
+    else
+      skip_authorization
+    end
   end
+
+  private
+
+  def recent_updates
+    updates = []
+    Person.order('updated_at desc').limit(5)
+          .each { |p| updates.push(p) }
+    Photo.order('updated_at desc').limit(5)
+         .each { |p| updates.push(p) }
+    Note.order('updated_at desc').limit(5)
+        .each { |n| updates.push(n) }
+
+    updates.sort_by!(&:updated_at).reverse!
+    updates = updates[0..4]
+    authorize_list updates
+    updates
+  end
+
 end
