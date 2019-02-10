5228f6b8e25fa6e4b5ed482fdd4a72c471b324f3
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-02-10 10:13:38 -0500

    clean up unused files and ignore certain files for coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter '/app/dashboards/'
+  add_filter '/app/controllers/admin/'
+  add_filter '/app/policies/'
+  add_filter '/app/fields/'
+  add_filter '/app/channels/'
+  add_filter '/app/mailers/'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
