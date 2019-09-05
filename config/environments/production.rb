2fc8d39629a69d7dadbd0a48a1703c5720545626
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-09-05 19:36:17 -0400

    Fix log level setup in production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,11 +50,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = Logger.const_get(
-    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
-  )
+  # Use the lowest log level by default to ensure availability of
+  # diagnostic information when problems arise.
+  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
