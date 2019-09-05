a6f4c31b23154a726ce0d0929c72bb50bf4e3d63
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2019-09-04 22:50:01 -0400

    Set log level with environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,9 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
