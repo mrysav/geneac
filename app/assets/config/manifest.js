526699caca0bbce14baedefcad400760da059e8e
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2020-01-04 16:46:10 -0500

    fix manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,6 @@
-{}
+//= link administrate/application.css
+//= link administrate/application.js
+//= link administrate-field-nested_has_many/application.css
+//= link administrate-field-nested_has_many/application.js
+{
+}
