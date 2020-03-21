9b0b8c96973d253e633951882332e536da3085b9
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2020-03-21 11:57:01 -0400

    Add factory_bot

---
+++ test/factories/facts.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :fact do
+    fact_type { %w[Residence Marriage Baptism Divorce] }
+    date_string do
+      Faker::Date.between(from: 50.years.ago, to: Date.today).strftime('%F')
+    end
+    place { Faker::Address.full_address }
+
+    trait :attached_to_person do
+      association :factable, factory: :person
+    end
+  end
+end
