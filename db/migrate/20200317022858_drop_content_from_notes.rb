587bfce88fe3d2e0d1c2c2c77d0280fbe5b382b2
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2020-05-13 22:33:04 -0400

    More resilient migration...

--- db/migrate/20200317022858_drop_content_from_notes.rb
+++ db/migrate/20200317022858_drop_content_from_notes.rb
@@ -1,5 +1,7 @@
 class DropContentFromNotes < ActiveRecord::Migration[6.0]
   def up
+    return unless Note.column_names.include? 'content'
+
     Note.all.each do |n|
       unless n.content.blank?
         throw 'Some notes still have content. Run `rake content:migrate_rich_text` and re-run these migrations.'
