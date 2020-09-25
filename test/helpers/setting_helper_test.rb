256aa9b69c9ac177f92a39ff8157765d3dbeb8fa
Emily Green <egreen@g2.com>
2020-09-25 17:22:29 -0500

    Migrate SettingHelper spec from minitest to rspec

--- test/helpers/setting_helper_test.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class SettingHelperTest < ActionView::TestCase
-  test 'should render a setting' do
-    Setting.site_title = 'xyzzy'
-    assert_equal 'xyzzy', setting(:site_title)
-  end
-end
