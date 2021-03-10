00da8a351b7dada7fe510dfeaa6200e03c5cd7ab
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2021-03-10 09:54:10 -0500

    Proxy ActiveStorage requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
 
+  # Proxy requests!
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
