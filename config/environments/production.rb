c17205089b4159332ffdfe58af00c069827e512d
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2021-08-13 11:25:15 -0400

    Remove PORT config for actionmailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :ses
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'],
-                                               port: ENV['PORT'] || 3000 }
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
