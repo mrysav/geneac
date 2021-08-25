95e56cc69f98d8fc3f690d47cb8af55a43f00515
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2021-08-25 19:37:51 -0400

    person_field to stimulus!

---
+++ app/javascript/controllers/person_field_controller.js
@@ -0,0 +1,70 @@
+import { Controller } from 'stimulus'
+import Awesomplete from 'awesomplete'
+import ajax from '../lib/ajax'
+
+const people_tags_url = '/ajax/people_tags'
+const person_tag_url = '/ajax/people_tag/'
+
+export default class extends Controller {
+  static targets = ['value']
+
+  /**
+   * Awesomplete-enabled element for inputting a single person.
+   * @param {Element} element
+   */
+  connect() {
+    const element = this.valueTarget
+
+    ajax.get(people_tags_url, (people) => {
+      let peopleList = JSON.parse(people)
+      let personSearchBox = element.querySelector('.person-field-search')
+      let personIdInput = element.querySelector('.person-field-id')
+      let selectedPersonElement = element.querySelector(
+        '.person-field-selected'
+      )
+      let personNameField = selectedPersonElement.querySelector('.name')
+      let closeBtn = selectedPersonElement.querySelector('i')
+
+      let populatePersonField = function (personId) {
+        personIdInput.value = personId
+        ajax.get(person_tag_url + personId, function (name) {
+          personNameField.textContent = name
+          selectedPersonElement.style.display = 'inline'
+          personSearchBox.style.display = 'none'
+          closeBtn.style.display = 'inline'
+        })
+      }
+
+      closeBtn.addEventListener('click', function (event) {
+        personIdInput.value = ''
+        selectedPersonElement.style.display = 'none'
+        personSearchBox.style.display = 'block'
+        closeBtn.style.display = 'none'
+      })
+
+      if (personIdInput.value) {
+        populatePersonField(personIdInput.value)
+      } else {
+        personSearchBox.style.display = 'block'
+      }
+
+      let initializeAwesomplete = function () {
+        return new Awesomplete(personSearchBox, {
+          list: peopleList,
+          replace: function () {
+            this.input.value = ''
+          },
+        })
+      }
+
+      personSearchBox.addEventListener(
+        'awesomplete-selectcomplete',
+        function (event) {
+          populatePersonField(event.text.value)
+        }
+      )
+
+      initializeAwesomplete()
+    })
+  }
+}
