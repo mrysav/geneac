88831ba12154832fd036cf627ed9bb8a33ffd907
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-07-07 18:35:46 +0000

    Move webpacker to shakapacker 6.0.2

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,11 @@
+const { webpackConfig, merge } = require('shakapacker')
+
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+
+const customConfig = {
+  resolve: {
+    extensions: ['.css'],
+  },
+}
+
+module.exports = merge(webpackConfig, customConfig)
