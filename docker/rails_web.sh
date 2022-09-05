692b15b9e145471e3714312722e83612d48c3992
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-09-05 12:04:45 -0400

    Build containers action

--- docker/rails_web.sh
+++ docker/rails_web.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-
-bundle exec rails server --port 3000 --binding 0.0.0.0
+bundle exec rails server --port "$PORT" --binding 0.0.0.0
