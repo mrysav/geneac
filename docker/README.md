6344a7d74f213f8bf34ddc9d66141812bdcddb84
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2022-12-18 01:31:52 +0000

    Remove a bunch of docker demo files

--- docker/README.md
+++
@@ -1,5 +0,0 @@
-# Docker
-
-The contents of this folder are unsupported files that might help you run Geneac in your own production-grade Docker container.
-
-They are possibly outdated.
--- docker/make_buckets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This file will use the minio command line client to create the 'geneac' bucket
-
-mc alias set minio "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
-mc mb --ignore-existing "minio/$MINIO_BUCKET_NAME"
-mc ls "minio/$MINIO_BUCKET_NAME"
