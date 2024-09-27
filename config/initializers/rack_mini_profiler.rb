3af0da7ccd47b036d0a18ff831be696a7350de2c
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2024-09-27 15:40:19 -0400

    disable profiler except in development

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-Rack::MiniProfiler.config.position = 'bottom-right'
+if Rails.env.development?
+  Rack::MiniProfiler.config.position = "bottom-right"
 
-redis_url = ENV['REDIS_URL']
+  redis_url = ENV.fetch("REDIS_URL", nil)
 
-if redis_url.present?
-  Rack::MiniProfiler.config.storage_options = { url: redis_url }
-  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  if redis_url.present?
+    Rack::MiniProfiler.config.storage_options = { url: redis_url }
+    Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  end
 end
