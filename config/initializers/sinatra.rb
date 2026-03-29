b22bba2ccad98184e644cd158b0c3105ed607f75
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2026-03-29 10:05:57 -0400

    patch in sinatra.rb again

---
+++ config/initializers/sinatra.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Still needs a monkey patch
+# https://github.com/resque/resque/issues/1908
+Rails.application.config.after_initialize do
+  require "sinatra/base"
+  class Sinatra::Base
+    set :host_authorization, permitted_hosts: Rails.application.config.hosts.map(&:to_s)
+  end
+end
