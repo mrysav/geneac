44f081d3f643f5af60ac01ff31f273e45cf15534
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2026-03-29 10:08:41 -0400

    Add localhost too

--- config/initializers/sinatra.rb
+++ config/initializers/sinatra.rb
@@ -5,6 +5,6 @@
 Rails.application.config.after_initialize do
   require "sinatra/base"
   class Sinatra::Base
-    set :host_authorization, permitted_hosts: Rails.application.config.hosts.map(&:to_s)
+    set :host_authorization, permitted_hosts: Rails.application.config.hosts.map(&:to_s).concat(["127.0.0.1", "localhost"])
   end
 end
